b9670defe8d02519cb52c13db258fbfa20295b63
hirotake111 <hirotake111@users.noreply.github.com>
2022-01-22 21:59:35 +0000

    added child component to map

--- components/RightColumn/RightColumn.tsx
+++ components/RightColumn/RightColumn.tsx
@@ -1,11 +1,14 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
 import GoogleMapReact from "google-map-react";
 
 import { useCoordinates } from "../../hooks/map";
+import { useAppSelector } from "../../redux/store";
 
 export default function RightColumn() {
   const { coordinates, setCoordinates, setBoundaries, zoom } =
     useCoordinates(true);
+
+  const { places } = useAppSelector((state) => state.search);
 
   return (
     <Box h="100%" bgColor="ThreeDDarkShadow">
@@ -17,15 +20,59 @@ export default function RightColumn() {
         // options={}
         onChange={(e) => {
           console.log("onChange");
-          // console.log(e);
           // set coordinates and boundaries
           setCoordinates({ ...e.center });
           setBoundaries({ ...e.bounds });
         }}
-        // onChildClick={}
-      ></GoogleMapReact>
+        onChildClick={(e) => {
+          console.log("child clicked!", e);
+        }}
+      >
+        {places.map((p, i) => {
+          return (
+            <MapChild
+              key={i}
+              lat={Number(p.latitude)}
+              lng={Number(p.longitude)}
+            >
+              {p.name}
+            </MapChild>
+          );
+        })}
+      </GoogleMapReact>
     </Box>
     // <div className={styles.rightColumn}>
     // </div>
   );
 }
+
+const MapChild = (props: { [key: string]: any }) => {
+  return (
+    <div {...props}>
+      <Box position="absolute">
+        <Flex justifyContent="center" alignItems="center">
+          <Box
+            position="relative"
+            // display="flex"
+            p="5px"
+            minW="80px"
+            maxW="100px"
+            borderRadius="15px"
+            whiteSpace="nowrap"
+            textOverflow="ellipsis"
+            overflow="hidden"
+            bgColor="white"
+            border="1px solid #eeeeee"
+            boxShadow="2px 2px 5.3px rgba(0, 0, 0, 0.028),
+            6.7px 6.7px 17.9px rgba(0, 0, 0, 0.042),
+            30px 30px 80px rgba(0, 0, 0, 0.07)"
+            cursor="pointer"
+            fontWeight="500"
+          >
+            {props.children}
+          </Box>
+        </Flex>
+      </Box>
+    </div>
+  );
+};
